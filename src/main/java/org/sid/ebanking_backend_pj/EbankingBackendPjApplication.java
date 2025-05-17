7aecacabba04ecd2d7cfa25bafcb6eff7df2e281
Hajarfallaki <hajarfallaki24@gmail.com>
2025-05-17 22:42:51 +0100

    Initial commit avec les packages

---
+++ src/main/java/org/sid/ebanking_backend_pj/EbankingBackendPjApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ebanking_backend_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBackendPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBackendPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/ebanking_backend_pj/EbankingBackendPjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.ebanking_backend_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingBackendPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
